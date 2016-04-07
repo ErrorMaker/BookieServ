8c01de1fab4b3f25d65be47bdc71f787fb55dc77
Scott Stamp <scott@hypermine.com>
2016-04-07 10:58:35 -0230

    Initial commit

---
+++ BookieServ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace BookieServ
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      var wssv = new WebSocketServer("ws://192.99.109.121:3000");
+      wssv.AddWebSocketService<BookieService>("/");
+      wssv.Start();
+      Console.ReadKey(true);
+      wssv.Stop();
+    }
+  }
+}
